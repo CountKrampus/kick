f081c6a3fa9abb0f44199f9f532ebaf6177d62c6
CountKrampus <34731746+CountKrampus@users.noreply.github.com>
2021-03-02 18:50:48 -0800

    Add files via upload

--- bot.js
+++ bot.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const { Client } = require('discord.js');
 const { registerCommands, registerEvents } = require('./utils/registry');
 const client = new Client();
+client.snipes = new Map();
 
 (async () => {
   client.commands = new Map();
@@ -11,4 +12,3 @@ const client = new Client();
   await registerEvents(client, '../events');
   await client.login(process.env.DISCORD_BOT_TOKEN);
 })();
-
